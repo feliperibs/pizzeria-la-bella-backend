fadf4b25dc5079056216152c29fec8d006553afb
Felipe Ribeiro <felipee.rib@gmail.com>
2023-12-03 20:54:32 +0000

    adding correct routes

--- netlify/functions/api.ts
+++ netlify/functions/api.ts
@@ -1,9 +1,9 @@
 import express, { Router } from "express";
 import serverless from "serverless-http";
+import { router } from "../../src/routes"
 
 const api = express();
 
-const router = Router();
 router.get("/hello", (req, res) => res.send("Hello World!"));
 
 api.use("/api/", router);
