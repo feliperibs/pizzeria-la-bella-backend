1f9b7b3ca9bb88bda8e2fac32d8d73b49718f0b6
Felipe Ribeiro <felipee.rib@gmail.com>
2023-12-03 17:24:48 +0000

    fixing config

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import dotenv from "dotenv";
 import { router } from "./routes";
 
 dotenv.config();
-const app = express();
+export const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
