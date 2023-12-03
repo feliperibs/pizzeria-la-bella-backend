e7c694e9e638fc91b061dea198b94895ea67b997
Felipe Ribeiro <felipee.rib@gmail.com>
2023-12-03 22:06:25 +0000

    trying fix

--- netlify/functions/api.ts
+++ netlify/functions/api.ts
@@ -7,28 +7,26 @@ import { router } from "../../src/routes";
 
 const api = express();
 router.get("/hello", (req, res) => res.send("Hello World!"));
+
 dotenv.config();
 const app = express();
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-export const handler = async (event) => {
-  try {
-    mongoose
-      .connect("mongodb+srv://felipeerib:gi5n4tPohI55gg7s@cluster0.yfo4rfk.mongodb.net/")
-      .then(() => {
-        app.get("/", (_req: express.Request, res: express.Response) => {
-          return res.send("Express Typescript on Vercel");
-        });
-        
-        app.use("/api", router);
-        return serverless(api)
-      })
-      .catch((err) => {
-        throw new Error(err);
-      });
-  } catch (error) {
-    throw new Error(error);
-  }
-};
+mongoose
+  .connect("mongodb+srv://felipeerib:gi5n4tPohI55gg7s@cluster0.yfo4rfk.mongodb.net/")
+  .then(() => {
+    app.get("/", (_req: express.Request, res: express.Response) => {
+      return res.send("Express Typescript on Vercel");
+    });
+
+    app.use("/api", router);
+  })
+  .catch((err) => {
+    throw new Error(err);
+  });
+
+export const handler = serverless(api);
+
