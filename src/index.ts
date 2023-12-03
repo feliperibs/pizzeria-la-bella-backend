4950440b904bb550b3ba2c3fcde2c98a4cbebede
Felipe Ribeiro <felipee.rib@gmail.com>
2023-12-03 17:13:05 +0000

    fixing vercel

--- src/index.ts
+++ src/index.ts
@@ -5,22 +5,21 @@ import dotenv from "dotenv";
 import { router } from "./routes";
 
 dotenv.config();
-export const app = express();
+const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const url =
-  "mongodb+srv://felipeerib:gi5n4tPohI55gg7s@cluster0.yfo4rfk.mongodb.net/";
+const url ="mongodb+srv://felipeerib:gi5n4tPohI55gg7s@cluster0.yfo4rfk.mongodb.net/";
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
 
 mongoose
   .connect(url)
   .then(() => {
-    app.listen(port, () => {
-      console.log(`Server running at http://localhost:${port}`);
-    });
-
     app.get("/", (_req: express.Request, res: express.Response) => {
       return res.send("Express Typescript on Vercel");
     });
@@ -28,4 +27,3 @@ mongoose
     app.use("/api", router);
   })
   .catch((err) => console.log("Database connection error", err));
-  
