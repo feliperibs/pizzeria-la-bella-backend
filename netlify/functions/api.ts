6443e298d857cf729e302e6d60401c35cf18392f
Felipe Ribeiro <felipee.rib@gmail.com>
2023-12-03 21:02:16 +0000

    adding mongo connection

--- netlify/functions/api.ts
+++ netlify/functions/api.ts
@@ -1,11 +1,34 @@
-import express, { Router } from "express";
+import express from "express";
 import serverless from "serverless-http";
-import { router } from "../../src/routes"
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
+import dotenv from "dotenv";
+import { router } from "../../src/routes";
 
 const api = express();
-
 router.get("/hello", (req, res) => res.send("Hello World!"));
-
-api.use("/api/", router);
-
 export const handler = serverless(api);
+
+dotenv.config();
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
+
+mongoose
+  .connect("mongodb+srv://felipeerib:gi5n4tPohI55gg7s@cluster0.yfo4rfk.mongodb.net/")
+  .then(() => {
+    app.get("/", (_req: express.Request, res: express.Response) => {
+      return res.send("Express Typescript on Vercel");
+    });
+
+    app.use("/api", router);
+  })
+  .catch((err) => {
+    throw new Error(err);
+  });
